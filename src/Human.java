520ae695567093af65d8aa713f8c8ec7856165fa
Ilyas <ilyas-sbrf@mail.ru>
2024-05-05 14:03:38 +0300

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,14 @@
+public class Human {
+    private int head;
+    private int hands;
+    private int legs;
+    private int eyes;
+
+    public void setHead(int head) {
+        this.head = head;
+    }
+
+    public int getHead() {
+        return head;
+    }
+}
---
+++ src/Man.java
@@ -0,0 +1,4 @@
+public class Man extends Human {
+    String beardColor;
+
+}
